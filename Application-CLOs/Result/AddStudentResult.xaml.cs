b07dc22f18d7ae89ab6b5f7c81fcd55f16b63bf4
Hammad Hassan <mhhjutt@gmail.com>
2023-02-25 17:45:41 +0500

    Design-Add Student Result

---
+++ Application-CLOs/Result/AddStudentResult.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Application_CLOs
+{
+    /// <summary>
+    /// Interaction logic for AddStudentResult.xaml
+    /// </summary>
+    public partial class AddStudentResult : Window
+    {
+        int assessmentId;
+        int studentId;
+        public AddStudentResult()
+        {
+            InitializeComponent();
+        }
+        public AddStudentResult(int studentId)
+        {
+            InitializeComponent();
+            this.studentId=studentId;
+            settingComboBox();
+        }
+        private void settingComboBox()
+        {
+            cmbxAssessmentName.SelectedIndex = 0;
+        }
+        private string queryData(string query)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand(query, con);
+            string output = cmd.ExecuteScalar().ToString();
+            return output;
+        }
+        private void bindAssessmentName()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT A.Title\r\nFROM Assessment A", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                // Add data to combo box
+                cmbxAssessmentName.Items.Add(reader["Title"].ToString());
+            }
+        }
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            string query = "SELECT CONCAT(S.FirstName,' ',S.LastName)\r\nFROM Student S\r\nWHERE ID=" + studentId;
+            txtbxStudentName.Text=queryData(query);
+            query = "SELECT S.RegistrationNumber \r\nFROM Student S\r\nWHERE ID=" + studentId;
+            txtbxRegistrationNumber.Text = queryData(query);
+            bindAssessmentName();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnViewRubric_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAssessmnet_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ Application-CLOs/Result/ViewStudentForMarkeResult.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Application_CLOs
+{
+    /// <summary>
+    /// Interaction logic for ViewStudentForMarkeResult.xaml
+    /// </summary>
+    public partial class ViewStudentForMarkeResult : Window
+    {
+        public ViewStudentForMarkeResult()
+        {
+            InitializeComponent();
+        }
+        private void bindDataGrid()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd1 = new SqlCommand("SELECT S.id,S.FirstName,S.lastname,S.Contact,S.email,S.RegistrationNumber,L.Name AS Status FROM Student S JOIN Lookup  L ON L.LookupId=S.Status WHERE  L.Category='STUDENT_STATUS'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd1);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgStudent.ItemsSource = dt.DefaultView;
+
+        }
+      
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            bindDataGrid();
+        }
+
+        private void btnAssessment_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dgStudent.SelectedIndex >= 0 && dgStudent.SelectedValue != null)
+                {
+                    DataRowView dataRowView = (DataRowView)((Button)e.Source).DataContext;
+                    int studentId = int.Parse(dataRowView[0].ToString());
+                    AddStudentResult addStudentResult = new AddStudentResult(studentId);
+                    addStudentResult.ShowDialog();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("You are trying to access the wrong field", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+          
+        }
+
+      
+    }
+}
